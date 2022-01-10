1c58b92d72382a479fdcd46ff6661defd4875170
Beth Jackson <beth.jack@gmail.com>
2022-01-10 14:49:21 +0100

    Set up CSS reset

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,56 @@
-import Head from "next/head";
 import type { NextPage } from "next";
 import styled, { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
+*, *::before, *::after {
+  box-sizing: border-box;
+}
+
+* {
+  margin: 0;
+}
+
+html, body {
+  height: 100%;
+}
+
 html {
   min-height: 100%;
         }
+
 body {
-          box-sizing: border-box;
-          background: #ffe6e6;
+  line-height: 1.5;
+  -webkit-font-smoothing: antialiased;
+          background: black;
+          color: white;
           font-family: Arial, Helvetica, sans-serif;
           font-size: 20px;
           font-weight: 200;
         }
+
+        img, picture, video, canvas, svg {
+  display: block;
+  max-width: 100%;
+}
+
+input, button, textarea, select {
+  font: inherit;
+}
+
+p, h1, h2, h3, h4, h5, h6 {
+  overflow-wrap: break-word;
+}
+
+#root, #__next {
+  isolation: isolate;
+}
+
 `;
 
 const Home: NextPage = () => {
   return (
     <Container>
+      <GlobalStyles />
       <Header>Welcome to Next.js!</Header>
     </Container>
   );
@@ -30,6 +63,4 @@ const Container = styled.div`
   flex-direction: column;
 `;
 
-const Header = styled.h1`
-  color: red;
-`;
+const Header = styled.h1``;
